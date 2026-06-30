8b51032b6dc86556adca1dbd72ceba5305849c45
DucLoi24 <ndlgaming2004@gmail.com>
2026-06-30 08:37:44 +0700

    CRUD MonHoc API

---
+++ QLSV/Controllers/MonHocController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLSV.Models;
+
+namespace QLSV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonHocController : ControllerBase
+    {
+        private readonly QLSVContext _db;
+
+        public MonHocController(QLSVContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMonHocs()
+        {
+            var ds = _db.MonHocs.ToList();
+            return Ok(ds);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMonHocById(string id)
+        {
+            var mh = _db.MonHocs.Find(id);
+            return Ok(mh);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMonHoc(MonHoc monHoc)
+        {
+            _db.MonHocs.Add(monHoc);
+            _db.SaveChanges();
+            return CreatedAtAction(nameof(GetMonHocById), new { id = monHoc.MaMh }, monHoc);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMonHoc(string id, MonHoc monHoc)
+        {
+            var mh = _db.MonHocs.Find(id);
+            if (mh == null)
+            {
+                return NotFound();
+            }
+            mh.MaKhoa = monHoc.MaKhoa;
+            mh.TenMh = monHoc.TenMh;
+            mh.SoTinChi = monHoc.SoTinChi;
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMonHoc(string id)
+        {
+            var mh = _db.MonHocs.Find(id);
+            if (mh == null)
+            {
+                return NotFound();
+            }
+            _db.MonHocs.Remove(mh);
+            _db.SaveChanges();
+            return NoContent();
+        }
+    }
+}
